4b44f25af3026e2c41c81285bd4e3209239968c5
rajeshwari krishnamurthy <rk633@afsconnect1.njit.edu>
2017-11-15 19:21:26 -0500

    Code for database connection completed

--- index.php
+++ index.php
@@ -9,9 +9,44 @@ define('CONNECTION', 'sql2.njit.edu');
 
 class dbConn {
 protected static $db;
-private function __construct() {
-	try  {
-	     self::$db = new PDO('mysql:host=' . CONNECTION .';DBNAME=' .
+  private function __construct() {
+       try  {
+	     self::$db = new PDO('mysql:host=' . CONNECTION .';DBNAME='
+	     .DATABASE, USERNAME, PASSWORD );
+	     self::$db->setAttribute( PDO::ATTR_ERRMODE,
+	     PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e)  {
+	     echo "Connection Error:" . $e->getMessage();
+	}
+}
+
+  public static function getConnection()  {
+       if (!self::$db)  {
+         new dbConn();
+       }
+       return self::$db;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
